77c4a41fc0df90e9eb88e01785d1feb4907e3850
Farkas Olivér <oli70963@gmail.com>
2024-10-03 06:46:30 +0000

    feature: add filter by floor

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,7 @@
-const students = require('../students.json');
+const students = require("../students.json");
 
 //Írj függvényt annak kiszűrésére, hogy a tanulók melyik emeleten laknak.
 function f1(floor) {
-   //TODO
+  return students.filter((student) => student.dormitoryFloor === floor);
 }
 module.exports = f1;
