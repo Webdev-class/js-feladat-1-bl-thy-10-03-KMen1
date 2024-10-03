dfb16ffbab795a4c0f9ff6899c06a8e7c4b0bfac
Farkas Olivér <oli70963@gmail.com>
2024-10-03 06:47:17 +0000

    feature: add mismatch filter

--- f3/f3.js
+++ f3/f3.js
@@ -1,9 +1,12 @@
-const students = require('../students.json');
+const students = require("../students.json");
 /* Írj függvényt ami megvizsgálja, hogy van-e eltérés a freeDormitory
- boolean és dormitoryPrice number-ek között 
+ boolean és dormitoryPrice number-ek között
  (Ha jól van feltöltve a lista a false értékek száma és a null-ok száma megegyezik.
   Ha nem akkor írja ki az eltérést) */
 const f3 = () => {
-    //TODO
-}
+  return students.filter(
+    (student) =>
+      student.freeDormitory === true && student.dormitoryPrice !== null
+  ).length;
+};
 module.exports = f3;
