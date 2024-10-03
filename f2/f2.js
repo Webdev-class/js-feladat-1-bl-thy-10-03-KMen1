849256d10863a17633e7cb0d3737d9963482c73b
Farkas Olivér <oli70963@gmail.com>
2024-10-03 06:46:58 +0000

    feature: add club selection

--- f2/f2.js
+++ f2/f2.js
@@ -1,8 +1,18 @@
-const students = require('../students.json');
-/* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon) 
+const students = require("../students.json");
+/* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon)
 a klubbok nevével és tagjainak számával. (pl. {"club1": 4, "club2": 3....}) */
 function f2() {
-    //TODO
+  const clubs = {};
+
+  students.forEach((student) => {
+    if (clubs[student.club]) {
+      clubs[student.club]++;
+    } else {
+      clubs[student.club] = 1;
+    }
+  });
+
+  return clubs;
 }
 
 module.exports = f2;
